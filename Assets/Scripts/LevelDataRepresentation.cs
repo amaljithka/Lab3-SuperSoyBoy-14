a589e06ed1c2c00108b2e133a00dd2e3be006b0d
Amaljith Kavungalparambil Ayyappan [Student] <amaljith.kavungalparambilayyappan@student.sl.on.ca>
2019-11-23 13:49:53 -0500

    Added json

---
+++ Assets/Scripts/LevelDataRepresentation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+[Serializable]
+
+public class LevelDataRepresentation
+{
+    public Vector2 playerStartPosition;
+    public CameraSettingsRepresentation cameraSettings;
+    public LevelItemRepresentation[] levelItems;
+
+    public Vector3 rotation;
+    public Vector3 scale;
+    public Vector2 position;
+    public string prefabName;
+    public int spriteOrder;
+    public string spriteLayer;
+    public Color spriteColor;
+}
